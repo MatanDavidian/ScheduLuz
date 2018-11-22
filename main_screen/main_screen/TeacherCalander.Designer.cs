963d1a033c7fc639d7882de9c55ae2eaa79d4e63
Matan <Matanda1@ac.sce.ac.il>
2018-11-23 00:57:12 +0200

    update

--- main_screen/main_screen/TeacherCalander.Designer.cs
+++ main_screen/main_screen/TeacherCalander.Designer.cs
@@ -28,12 +28,148 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.add_event = new System.Windows.Forms.Button();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Location = new System.Drawing.Point(28, 51);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(243, 350);
+            this.listView1.TabIndex = 21;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(318, 352);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(125, 50);
+            this.button9.TabIndex = 20;
+            this.button9.Text = "button9";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(318, 273);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(125, 50);
+            this.button8.TabIndex = 19;
+            this.button8.Text = "button8";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(481, 352);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(125, 50);
+            this.button7.TabIndex = 18;
+            this.button7.Text = "button7";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(481, 273);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(125, 50);
+            this.button6.TabIndex = 17;
+            this.button6.Text = "button6";
+            this.button6.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(640, 352);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(125, 50);
+            this.button5.TabIndex = 16;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(640, 271);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(125, 50);
+            this.button4.TabIndex = 15;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(640, 194);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(125, 50);
+            this.button3.TabIndex = 14;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(640, 120);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(125, 50);
+            this.button2.TabIndex = 13;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // add_event
+            // 
+            this.add_event.Location = new System.Drawing.Point(640, 49);
+            this.add_event.Name = "add_event";
+            this.add_event.Size = new System.Drawing.Size(125, 50);
+            this.add_event.TabIndex = 12;
+            this.add_event.Text = "Add event";
+            this.add_event.UseVisualStyleBackColor = true;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(299, 99);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 11;
+            // 
+            // TeacherCalander
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.add_event);
+            this.Controls.Add(this.monthCalendar1);
+            this.Name = "TeacherCalander";
             this.Text = "TeacherCalander";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button add_event;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
     }
 }
