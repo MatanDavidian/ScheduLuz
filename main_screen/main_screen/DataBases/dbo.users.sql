b57ad26b1c00aba5e26d9912e45924906fd27a8f
TomSce <tomda@ac.sce.ac.il>
2018-11-25 13:16:00 +0200

    Tom - added 2 users to the connection_details database

---
+++ main_screen/main_screen/DataBases/dbo.users.sql
@@ -0,0 +1,17 @@
+﻿CREATE TABLE [dbo].[users] (
+    [Id]          INT         NOT NULL,
+    [name]        NCHAR (20)  NOT NULL,
+    [lastName]    NCHAR (20)  NOT NULL,
+    [permission]  NCHAR (10)  NOT NULL,
+    [phoneNumber] INT         NOT NULL,
+    [Email]       NCHAR (30)  NOT NULL,
+    [address]     NCHAR (30)  NOT NULL,
+    [birthDate]   DATETIME    NOT NULL,
+    [grade]       NCHAR (1)   NULL,
+    [classNumber] INT         NULL,
+    [hobies]      NCHAR (255) NULL,
+    [free Text]   NCHAR (255) NULL,
+    [picture]     IMAGE       NULL,
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
