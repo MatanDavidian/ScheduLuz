f724e093d02925d4199e725f2e1fd2cd3c4c18fe
tom <tomda@ac.sce.ac.il>
2018-12-24 00:02:47 +0200

    graphic

--- main_screen/main_screen/Manager/ManagerCalander.Designer.cs
+++ main_screen/main_screen/Manager/ManagerCalander.Designer.cs
@@ -57,88 +57,135 @@
             // 
             // add_user
             // 
+            this.add_user.BackColor = System.Drawing.Color.Transparent;
+            this.add_user.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("add_user.BackgroundImage")));
+            this.add_user.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.add_user.FlatAppearance.BorderSize = 0;
+            this.add_user.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.add_user.Location = new System.Drawing.Point(547, 12);
             this.add_user.Name = "add_user";
             this.add_user.Size = new System.Drawing.Size(125, 30);
             this.add_user.TabIndex = 1;
             this.add_user.Text = "Add user";
-            this.add_user.UseVisualStyleBackColor = true;
+            this.add_user.UseVisualStyleBackColor = false;
             this.add_user.Click += new System.EventHandler(this.button1_Click);
             // 
             // Add_event
             // 
+            this.Add_event.BackColor = System.Drawing.Color.Transparent;
+            this.Add_event.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Add_event.BackgroundImage")));
+            this.Add_event.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Add_event.FlatAppearance.BorderSize = 0;
+            this.Add_event.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Add_event.Location = new System.Drawing.Point(547, 48);
             this.Add_event.Name = "Add_event";
             this.Add_event.Size = new System.Drawing.Size(125, 30);
             this.Add_event.TabIndex = 2;
             this.Add_event.Text = "Add event";
-            this.Add_event.UseVisualStyleBackColor = true;
+            this.Add_event.UseVisualStyleBackColor = false;
             this.Add_event.Click += new System.EventHandler(this.button2_Click);
             // 
             // Profile_btn
             // 
+            this.Profile_btn.BackColor = System.Drawing.Color.Transparent;
+            this.Profile_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Profile_btn.BackgroundImage")));
+            this.Profile_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Profile_btn.FlatAppearance.BorderSize = 0;
+            this.Profile_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Profile_btn.Location = new System.Drawing.Point(547, 84);
             this.Profile_btn.Name = "Profile_btn";
             this.Profile_btn.Size = new System.Drawing.Size(125, 30);
             this.Profile_btn.TabIndex = 3;
             this.Profile_btn.Text = "Profile";
-            this.Profile_btn.UseVisualStyleBackColor = true;
+            this.Profile_btn.UseVisualStyleBackColor = false;
             this.Profile_btn.Click += new System.EventHandler(this.Profile_btn_Click);
             // 
             // BBorad_btn
             // 
+            this.BBorad_btn.BackColor = System.Drawing.Color.Transparent;
+            this.BBorad_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("BBorad_btn.BackgroundImage")));
+            this.BBorad_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.BBorad_btn.FlatAppearance.BorderSize = 0;
+            this.BBorad_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.BBorad_btn.Location = new System.Drawing.Point(547, 120);
             this.BBorad_btn.Name = "BBorad_btn";
             this.BBorad_btn.Size = new System.Drawing.Size(125, 30);
             this.BBorad_btn.TabIndex = 4;
             this.BBorad_btn.Text = "Bulletin Board";
-            this.BBorad_btn.UseVisualStyleBackColor = true;
+            this.BBorad_btn.UseVisualStyleBackColor = false;
             this.BBorad_btn.Click += new System.EventHandler(this.BBorad_btn_Click);
             // 
             // fixed_btn
             // 
+            this.fixed_btn.BackColor = System.Drawing.Color.Transparent;
+            this.fixed_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("fixed_btn.BackgroundImage")));
+            this.fixed_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.fixed_btn.FlatAppearance.BorderSize = 0;
+            this.fixed_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.fixed_btn.Location = new System.Drawing.Point(547, 156);
             this.fixed_btn.Name = "fixed_btn";
             this.fixed_btn.Size = new System.Drawing.Size(125, 30);
             this.fixed_btn.TabIndex = 5;
             this.fixed_btn.Text = "Fixed Schedule";
-            this.fixed_btn.UseVisualStyleBackColor = true;
+            this.fixed_btn.UseVisualStyleBackColor = false;
             // 
             // contact_page_btn
             // 
+            this.contact_page_btn.BackColor = System.Drawing.Color.Transparent;
+            this.contact_page_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("contact_page_btn.BackgroundImage")));
+            this.contact_page_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.contact_page_btn.FlatAppearance.BorderSize = 0;
+            this.contact_page_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.contact_page_btn.Location = new System.Drawing.Point(547, 192);
             this.contact_page_btn.Name = "contact_page_btn";
             this.contact_page_btn.Size = new System.Drawing.Size(125, 30);
             this.contact_page_btn.TabIndex = 6;
             this.contact_page_btn.Text = "Contact Page";
-            this.contact_page_btn.UseVisualStyleBackColor = true;
+            this.contact_page_btn.UseVisualStyleBackColor = false;
             // 
             // public_event_btn
             // 
+            this.public_event_btn.BackColor = System.Drawing.Color.Transparent;
+            this.public_event_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("public_event_btn.BackgroundImage")));
+            this.public_event_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.public_event_btn.FlatAppearance.BorderSize = 0;
+            this.public_event_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.public_event_btn.Location = new System.Drawing.Point(547, 228);
             this.public_event_btn.Name = "public_event_btn";
             this.public_event_btn.Size = new System.Drawing.Size(125, 30);
             this.public_event_btn.TabIndex = 7;
             this.public_event_btn.Text = "Public Event";
-            this.public_event_btn.UseVisualStyleBackColor = true;
+            this.public_event_btn.UseVisualStyleBackColor = false;
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button8.Location = new System.Drawing.Point(547, 264);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(125, 30);
             this.button8.TabIndex = 8;
             this.button8.Text = "button8";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
             // 
             // button9
             // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.BackgroundImage = global::main_screen.Properties.Resources.bluebtn;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button9.Location = new System.Drawing.Point(547, 419);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(125, 30);
             this.button9.TabIndex = 9;
             this.button9.Text = "Log out";
-            this.button9.UseVisualStyleBackColor = true;
+            this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // motd
@@ -198,6 +245,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::main_screen.Properties.Resources.bg2;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(684, 461);
             this.Controls.Add(this.listView1);
             this.Controls.Add(this.motd);
@@ -211,6 +260,7 @@
             this.Controls.Add(this.Add_event);
             this.Controls.Add(this.add_user);
             this.Controls.Add(this.monthCalendar1);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "ManagerCalander";
             this.Text = "Calander - Manager";
--- main_screen/main_screen/Student/StudentCalander.Designer.cs
+++ main_screen/main_screen/Student/StudentCalander.Designer.cs
@@ -80,92 +80,164 @@
             // 
             // button9
             // 
+            this.button9.BackColor = System.Drawing.Color.Transparent;
+            this.button9.BackgroundImage = global::main_screen.Properties.Resources.bluebtn;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button9.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button9.Location = new System.Drawing.Point(547, 419);
             this.button9.Name = "button9";
             this.button9.Size = new System.Drawing.Size(125, 30);
             this.button9.TabIndex = 20;
             this.button9.Text = "Log out";
-            this.button9.UseVisualStyleBackColor = true;
+            this.button9.UseVisualStyleBackColor = false;
             this.button9.Click += new System.EventHandler(this.button9_Click);
             // 
             // button8
             // 
+            this.button8.BackColor = System.Drawing.Color.Transparent;
+            this.button8.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button8.BackgroundImage")));
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button8.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button8.Location = new System.Drawing.Point(547, 264);
             this.button8.Name = "button8";
             this.button8.Size = new System.Drawing.Size(125, 30);
             this.button8.TabIndex = 19;
             this.button8.Text = "Schedule a meeting";
-            this.button8.UseVisualStyleBackColor = true;
+            this.button8.UseVisualStyleBackColor = false;
             this.button8.Click += new System.EventHandler(this.button8_Click);
             // 
             // add_hw_btn
             // 
+            this.add_hw_btn.BackColor = System.Drawing.Color.Transparent;
+            this.add_hw_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("add_hw_btn.BackgroundImage")));
+            this.add_hw_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.add_hw_btn.FlatAppearance.BorderSize = 0;
+            this.add_hw_btn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.add_hw_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.add_hw_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.add_hw_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.add_hw_btn.Location = new System.Drawing.Point(547, 228);
             this.add_hw_btn.Name = "add_hw_btn";
             this.add_hw_btn.Size = new System.Drawing.Size(125, 30);
             this.add_hw_btn.TabIndex = 18;
             this.add_hw_btn.Text = "Add H.W";
-            this.add_hw_btn.UseVisualStyleBackColor = true;
+            this.add_hw_btn.UseVisualStyleBackColor = false;
             this.add_hw_btn.Click += new System.EventHandler(this.add_hw_btn_Click);
             // 
             // my_dear_diary_btn
             // 
+            this.my_dear_diary_btn.BackColor = System.Drawing.Color.Transparent;
+            this.my_dear_diary_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("my_dear_diary_btn.BackgroundImage")));
+            this.my_dear_diary_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.my_dear_diary_btn.FlatAppearance.BorderSize = 0;
+            this.my_dear_diary_btn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.my_dear_diary_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.my_dear_diary_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.my_dear_diary_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.my_dear_diary_btn.Location = new System.Drawing.Point(547, 192);
             this.my_dear_diary_btn.Name = "my_dear_diary_btn";
             this.my_dear_diary_btn.Size = new System.Drawing.Size(125, 30);
             this.my_dear_diary_btn.TabIndex = 17;
             this.my_dear_diary_btn.Text = "My Dear Diary";
-            this.my_dear_diary_btn.UseVisualStyleBackColor = true;
+            this.my_dear_diary_btn.UseVisualStyleBackColor = false;
             this.my_dear_diary_btn.Click += new System.EventHandler(this.my_dear_diary_btn_Click);
             // 
             // mail_btn
             // 
+            this.mail_btn.BackColor = System.Drawing.Color.Transparent;
+            this.mail_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("mail_btn.BackgroundImage")));
+            this.mail_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.mail_btn.FlatAppearance.BorderSize = 0;
+            this.mail_btn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.mail_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.mail_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.mail_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.mail_btn.Location = new System.Drawing.Point(547, 156);
             this.mail_btn.Name = "mail_btn";
             this.mail_btn.Size = new System.Drawing.Size(125, 30);
             this.mail_btn.TabIndex = 16;
             this.mail_btn.Text = "Mail";
-            this.mail_btn.UseVisualStyleBackColor = true;
+            this.mail_btn.UseVisualStyleBackColor = false;
             this.mail_btn.Click += new System.EventHandler(this.mail_btn_Click);
             // 
             // button4
             // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button4.BackgroundImage")));
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button4.Location = new System.Drawing.Point(547, 120);
             this.button4.Name = "button4";
             this.button4.Size = new System.Drawing.Size(125, 30);
             this.button4.TabIndex = 15;
             this.button4.Text = "Multiplayer event";
-            this.button4.UseVisualStyleBackColor = true;
+            this.button4.UseVisualStyleBackColor = false;
             this.button4.Click += new System.EventHandler(this.button4_Click);
             // 
             // Profile_btn
             // 
+            this.Profile_btn.BackColor = System.Drawing.Color.Transparent;
+            this.Profile_btn.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Profile_btn.BackgroundImage")));
+            this.Profile_btn.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Profile_btn.FlatAppearance.BorderSize = 0;
+            this.Profile_btn.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.Profile_btn.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.Profile_btn.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.Profile_btn.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.Profile_btn.Location = new System.Drawing.Point(547, 84);
             this.Profile_btn.Name = "Profile_btn";
             this.Profile_btn.Size = new System.Drawing.Size(125, 30);
             this.Profile_btn.TabIndex = 14;
             this.Profile_btn.Text = "Profile";
-            this.Profile_btn.UseVisualStyleBackColor = true;
+            this.Profile_btn.UseVisualStyleBackColor = false;
             this.Profile_btn.Click += new System.EventHandler(this.Profile_btn_Click);
             // 
             // contact_page
             // 
+            this.contact_page.BackColor = System.Drawing.Color.Transparent;
+            this.contact_page.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("contact_page.BackgroundImage")));
+            this.contact_page.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.contact_page.FlatAppearance.BorderSize = 0;
+            this.contact_page.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.contact_page.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.contact_page.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.contact_page.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.contact_page.Location = new System.Drawing.Point(547, 48);
             this.contact_page.Name = "contact_page";
             this.contact_page.Size = new System.Drawing.Size(125, 30);
             this.contact_page.TabIndex = 13;
             this.contact_page.Text = "Contact Page";
-            this.contact_page.UseVisualStyleBackColor = true;
+            this.contact_page.UseVisualStyleBackColor = false;
             this.contact_page.Click += new System.EventHandler(this.contact_page_Click);
             // 
             // add_event
             // 
+            this.add_event.BackColor = System.Drawing.Color.Transparent;
+            this.add_event.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("add_event.BackgroundImage")));
+            this.add_event.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.add_event.FlatAppearance.BorderSize = 0;
+            this.add_event.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.add_event.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.add_event.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.add_event.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.add_event.Location = new System.Drawing.Point(547, 12);
             this.add_event.Name = "add_event";
             this.add_event.Size = new System.Drawing.Size(125, 30);
             this.add_event.TabIndex = 12;
             this.add_event.Text = "Add event";
-            this.add_event.UseVisualStyleBackColor = true;
+            this.add_event.UseVisualStyleBackColor = false;
             this.add_event.Click += new System.EventHandler(this.add_event_Click);
             // 
             // monthCalendar1
@@ -182,7 +254,7 @@
             this.motd.Controls.Add(this.motd_txt);
             this.motd.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.motd.ForeColor = System.Drawing.Color.AliceBlue;
-            this.motd.Location = new System.Drawing.Point(245, 228);
+            this.motd.Location = new System.Drawing.Point(245, 26);
             this.motd.Name = "motd";
             this.motd.Size = new System.Drawing.Size(295, 221);
             this.motd.TabIndex = 22;
@@ -203,18 +275,28 @@
             // 
             // button1
             // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("button1.BackgroundImage")));
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.button1.Location = new System.Drawing.Point(547, 343);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 30);
             this.button1.TabIndex = 23;
             this.button1.Text = "Answer Surveys";
-            this.button1.UseVisualStyleBackColor = true;
+            this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // StudentCalander
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::main_screen.Properties.Resources.bg4;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(684, 461);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.motd);
@@ -229,6 +311,7 @@
             this.Controls.Add(this.contact_page);
             this.Controls.Add(this.add_event);
             this.Controls.Add(this.monthCalendar1);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "StudentCalander";
             this.Text = "Calander - Student";
