1463a2442a88f350143f0e939ec95e9acae7309c
MatanDavidian <davidianmatan@gmail.com>
2018-12-27 11:29:54 +0200

    request to cancel on work

--- main_screen/main_screen/Resources/dataBaseLocation.cs
+++ main_screen/main_screen/Resources/dataBaseLocation.cs
@@ -20,7 +20,7 @@ namespace database_location
     {
         public SqlConnection connect_to_scheduluz_DB()
         {
-            return new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=D:\projectC#\ScheduLuz2\main_screen\main_screen\ScheduLuz.mdf;Integrated Security=True;Connect Timeout=30");
+            return new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\isodot_c#\ScheduLuz2\main_screen\main_screen\ScheduLuz.mdf;Integrated Security=True;Connect Timeout=30");
         }
     }
     /*public class RoundButton : Button
