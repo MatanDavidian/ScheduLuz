78e1f38ec16d502c80e49d9066a01f183f046c15
amird92 <amird234@gmail.com>
2018-12-27 23:00:13 +0200

    Amir: Updated the background & buttons of My Schedule Form

--- main_screen/main_screen/Student/My schedule.Designer.cs
+++ main_screen/main_screen/Student/My schedule.Designer.cs
@@ -28,12 +28,14 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(My_schedule));
             this.button2 = new System.Windows.Forms.Button();
             this.title = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // button2
             // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
             this.button2.BackgroundImage = global::main_screen.Properties.Resources.bluebtn;
             this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ActiveBorder;
@@ -47,15 +49,16 @@
             this.button2.Size = new System.Drawing.Size(84, 34);
             this.button2.TabIndex = 79;
             this.button2.Text = "Return";
-            this.button2.UseVisualStyleBackColor = true;
+            this.button2.UseVisualStyleBackColor = false;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // title
             // 
             this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
             this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold);
-            this.title.ForeColor = System.Drawing.Color.RoyalBlue;
-            this.title.Location = new System.Drawing.Point(19, 9);
+            this.title.ForeColor = System.Drawing.Color.Black;
+            this.title.Location = new System.Drawing.Point(4, 9);
             this.title.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
             this.title.Name = "title";
             this.title.Size = new System.Drawing.Size(223, 39);
@@ -66,12 +69,14 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::main_screen.Properties.Resources.bg_student;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(910, 521);
             this.Controls.Add(this.title);
             this.Controls.Add(this.button2);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "My_schedule";
-            this.Text = "My_schedule";
+            this.Text = "My Schedule";
             this.Load += new System.EventHandler(this.My_schedule_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
