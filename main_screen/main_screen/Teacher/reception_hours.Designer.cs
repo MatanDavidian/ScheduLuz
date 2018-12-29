8ca4de17e422e1b18efc00cb0ad3e52c0f20ec36
amird92 <amird234@gmail.com>
2018-12-29 15:51:38 +0200

    Amir - 'reception hours' has been updated.

--- main_screen/main_screen/Teacher/reception_hours.Designer.cs
+++ main_screen/main_screen/Teacher/reception_hours.Designer.cs
@@ -38,17 +38,19 @@
             // title
             // 
             this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.Color.Transparent;
             this.title.Font = new System.Drawing.Font("Microsoft Sans Serif", 26.25F, System.Drawing.FontStyle.Bold);
-            this.title.ForeColor = System.Drawing.Color.Maroon;
+            this.title.ForeColor = System.Drawing.Color.Black;
             this.title.Location = new System.Drawing.Point(19, 9);
             this.title.Margin = new System.Windows.Forms.Padding(10, 0, 10, 0);
             this.title.Name = "title";
-            this.title.Size = new System.Drawing.Size(271, 39);
+            this.title.Size = new System.Drawing.Size(290, 39);
             this.title.TabIndex = 5;
-            this.title.Text = "reception hours";
+            this.title.Text = "Reception Hours";
             // 
             // Return
             // 
+            this.Return.BackColor = System.Drawing.Color.Transparent;
             this.Return.BackgroundImage = global::main_screen.Properties.Resources.bluebtn;
             this.Return.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Return.FlatAppearance.BorderSize = 0;
@@ -62,11 +64,12 @@
             this.Return.Size = new System.Drawing.Size(84, 34);
             this.Return.TabIndex = 76;
             this.Return.Text = "Return";
-            this.Return.UseVisualStyleBackColor = true;
+            this.Return.UseVisualStyleBackColor = false;
             this.Return.Click += new System.EventHandler(this.button2_Click);
             // 
             // Submit
             // 
+            this.Submit.BackColor = System.Drawing.Color.Transparent;
             this.Submit.BackgroundImage = global::main_screen.Properties.Resources.greenbtn;
             this.Submit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.Submit.FlatAppearance.BorderSize = 0;
@@ -80,32 +83,35 @@
             this.Submit.Size = new System.Drawing.Size(84, 34);
             this.Submit.TabIndex = 77;
             this.Submit.Text = "Submit";
-            this.Submit.UseVisualStyleBackColor = true;
+            this.Submit.UseVisualStyleBackColor = false;
             this.Submit.Click += new System.EventHandler(this.button3_Click);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.BackColor = System.Drawing.Color.DodgerBlue;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(177)));
             this.label1.Location = new System.Drawing.Point(78, 88);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(179, 13);
+            this.label1.Size = new System.Drawing.Size(222, 18);
             this.label1.TabIndex = 78;
-            this.label1.Text = "Choose yours reception hours.";
+            this.label1.Text = "Check your reception hours:";
             // 
             // reception_hours
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::main_screen.Properties.Resources.bg_worker;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.ClientSize = new System.Drawing.Size(855, 441);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.Submit);
             this.Controls.Add(this.Return);
             this.Controls.Add(this.title);
+            this.DoubleBuffered = true;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "reception_hours";
-            this.Text = "reception_hours";
+            this.Text = "Reception Hours";
             this.Load += new System.EventHandler(this.reception_hours_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
